7143db214274a767cba549330957c933bfd18d33
Eric Jenkins <eric@exeldigital.com>
2016-12-16 10:59:32 -0600

    PS-500. Addressing issues pointed out by Tere
    - Adding entrypoint conditionals to address if the uid:gid of the
       project directory is set to 0:0.

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,14 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-# Change user:group id values for 'nginx:nginx' to '1000:1000':
-usermod -u $(stat -c '%u' /var/www/html) nginx
-groupmod -g $(stat -c '%u' /var/www/html) nginx
-find /var/www -user 104 -exec chown -h nginx {} \;
-find /var/www -group 107 -exec chgrp -h nginx {} \;
+# Change user:group id values for 'www-data:www-data' to match the uid:gid of
+# the project docroot, unless the uid:gid are set to 0:0.
+USER_ID=$(stat -c '%u' /var/www/html)
+GROUP_ID=$(stat -c '%g' /var/www/html)
+if [ $USER_ID != 0 ]; then
+  usermod -u $USER_ID nginx
+  find /var/www -user 104 -exec chown -h nginx {} \;
+fi
+if [ $GROUP_ID != 0 ]; then
+  groupmod -g $GROUP_ID nginx
+  find /var/www -group 107 -exec chgrp -h nginx {} \;
+fi
 
-# If entrypoint.sh is called without arguments, then execute php-fpm service:
-if [ -n "$1" ]; then
+if [ $1 = "nginx" ]; then
+  # If first arg is `nginx`, then run as user root.
+  exec "$@"
+elif [ -n $1 ]; then
+  # If first arg is anything else, then execute as user "nginx".
   exec /usr/local/bin/gosu nginx "$@"
 else
-  nginx -g "daemon off;"
+  # If entrypoint.sh is called with no arg, execute as user root.
+  exec "$@"
 fi
--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-# Change user:group id values for 'www-data:www-data' to '1000:1000':
-usermod -u $(stat -c '%u' /var/www/html) www-data
-groupmod -g $(stat -c '%u' /var/www/html) www-data
-find /var/www -group 33 -exec chgrp -h www-data {} \;
+# Change user:group id values for 'www-data:www-data' to match the uid:gid of
+# the project docroot, unless the uid:gid are set to 0:0.
+USER_ID=$(stat -c '%u' /var/www/html)
+GROUP_ID=$(stat -c '%g' /var/www/html)
+if [ $USER_ID != 0 ]; then
+  usermod -u $USER_ID www-data
+fi
+if [ $GROUP_ID != 0 ]; then
+  groupmod -g $GROUP_ID www-data
+  find /var/www -group 33 -exec chgrp -h www-data {} \;
+fi
 
-# If entrypoint.sh is called without arguments, then execute php-fpm service:
-if [ -n "$1" ]; then
+if [ $1 = "php" ] || [ $1 = "php-fpm" ]; then
+  # If first arg is `php` or `php-fpm`, then run as user root.
+  exec "$@"
+elif [ -n $1 ]; then
+  # If first arg is anything else, then execute as user "www-data".
   exec /usr/local/bin/gosu www-data "$@"
 else
-  exec php-fpm
+  # If entrypoint.sh is called with no arg, execute as user root.
+  exec "$@"
 fi
